880b4764df5a6f8853b36329681573dc1e9f49c9
Sebastien Malissard <sebastien.malissard@dbmail.com>
2024-09-15 10:55:11 +0200

    Add wireguard-server

---
+++ wireguard-server/start_container.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+export NAME=${1:-wireguard-server}
+export CONFIG_PATH=${2:-$PATH/config}
+
+if [ -z "${NAME}" ] || [ -z "${CONFIG_PATH}" ]; then
+    echo "Invalid arguments";
+    return 1;
+fi
+
+docker-compose up -d
